798c179531163845d394450ec17a386bf2db1023
SyedDevop <syeds.devops007@gmail.com>
2025-04-23 02:01:03 +0530

    refactor savePixelsASPPM handle u8 and u32 ptr array

--- src/screenshot.zig
+++ src/screenshot.zig
@@ -7,20 +7,41 @@ const c = @cImport({
     @cInclude("X11/Xutil.h");
 });
 
-fn savePixelsAsPPM(pixels: [*]const u32, width: usize, height: usize, filepath: [:0]const u8) !void {
+/// Represents a generic pixel buffer that can be either a pointer to a constant
+/// array of 8-bit values (`u8`) or 32-bit values (`u32`).
+const Pixels = union(enum) {
+    /// Pointer to an array of constant 8-bit pixel values.
+    u8: [*]const u8,
+
+    /// Pointer to an array of constant 32-bit pixel values.
+    u32: [*]const u32,
+};
+
+fn savePixelsAsPPM(pixels: Pixels, width: usize, height: usize, filepath: [:0]const u8) !void {
     var file = try std.fs.cwd().createFileZ(filepath, .{});
     defer file.close();
 
     var buff_writer = std.io.bufferedWriter(file.writer());
     const writer = buff_writer.writer();
     try writer.print("P6\n{} {}\n255\n", .{ width, height });
-
-    for (0..(width * height)) |i| {
-        const pixel = pixels[i];
-        const c1: u8 = @intCast(pixel & 0xFF);
-        const c2: u8 = @intCast((pixel >> 8) & 0xFF);
-        const c3: u8 = @intCast((pixel >> 16) & 0xFF);
-        try writer.writeAll(&[_]u8{ c3, c2, c1 });
+    switch (pixels) {
+        .u8 => |p| {
+            for (0..(width * height)) |i| {
+                const c1: u8 = p[i * 4 + 0];
+                const c2: u8 = p[i * 4 + 1];
+                const c3: u8 = p[i * 4 + 2];
+                try writer.writeAll(&[_]u8{ c3, c2, c1 });
+            }
+        },
+        .u32 => |p| {
+            for (0..(width * height)) |i| {
+                const pixel = p[i];
+                const c1: u8 = @intCast(pixel & 0xFF);
+                const c2: u8 = @intCast((pixel >> 8) & 0xFF);
+                const c3: u8 = @intCast((pixel >> 16) & 0xFF);
+                try writer.writeAll(&[_]u8{ c3, c2, c1 });
+            }
+        },
     }
     try buff_writer.flush();
     try file.sync();
@@ -53,8 +74,17 @@ pub fn main() !void {
 
     const data = img.*.data;
     const pixel_ptr: [*]const u32 = @alignCast(@ptrCast(data));
+    const pixel_ptr_u8: [*]const u8 = @alignCast(@ptrCast(data));
 
     const width: usize = @intCast(img.*.width);
     const height: usize = @intCast(img.*.height);
-    try savePixelsAsPPM(pixel_ptr, width, height, "screen.ppm");
+    var timer = try std.time.Timer.start();
+    try savePixelsAsPPM(.{ .u32 = pixel_ptr }, width, height, "screen-u32.ppm");
+    var end = timer.read() / 1000_000;
+    std.debug.print("Save time for screen-u32: {d:.2} ms\n", .{end});
+
+    timer.reset();
+    try savePixelsAsPPM(.{ .u8 = pixel_ptr_u8 }, width, height, "screen-u8.ppm");
+    end = timer.read() / 1000_000;
+    std.debug.print("Save time for screen-u8: {d:.2} ms\n", .{end});
 }
